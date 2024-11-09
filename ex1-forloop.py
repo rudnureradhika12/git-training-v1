e77674f35e2cf483075de26cc4af554160c17535
radhika <radhikarudnure12@gmail.com>
2024-11-09 05:52:13 +0530

    2nd commit: modified ex1 and ex2 python files

---
+++ ex1-forloop.py
@@ -0,0 +1,11 @@
+#count to n and print each number
+n = int(input("Enter the number: "))
+def count_to_n(x = n):
+    for i in range(1, x+1):
+        print(1, end=' ')
+    print()
+
+print("Going to count to {} . . . . .".format(n))
+count_to_n(n)
+# --------------------------------------------------------------
+print("first modification for 2nd count")
---
+++ ex2-dfunction.py
@@ -0,0 +1,7 @@
+def double(n):
+    return 2 * n
+print("Teach the world online Create an online video course, reach students across the globe, and earn money Top companies choose Udemy Business to build in-demand career skills. NasdaqVolkswagenBoxNetAppEventbrite Explore top skills and certifications")
+
+print("The program")
+# -----------------------------------------------------------------------------------------
+print("first modification for 2nd program")
